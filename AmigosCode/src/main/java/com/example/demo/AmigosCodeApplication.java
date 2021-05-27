f7e27f71ec2539b122325adae8c9e7b6d3bb51c0
AngelAbraham <angel-07061@hotmail.com>
2021-05-26 22:46:52 -0500

    Api layer

--- AmigosCode/src/main/java/com/example/demo/AmigosCodeApplication.java
+++ AmigosCode/src/main/java/com/example/demo/AmigosCodeApplication.java
@@ -1,31 +1,13 @@
 package com.example.demo;
 
-import java.time.LocalDate;
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.example.demo.Student.Student;
 
 @SpringBootApplication
-@RestController
 public class AmigosCodeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AmigosCodeApplication.class, args);
 	}
 
-	@GetMapping
-	public List<Student> hello() {
-		
-		return List.of(new Student(
-				21L, 
-				"adasd",
-				"asdad", 
-				null, 
-				23));
-	}
 }
