99dc6ed333fb03bcfbb51126771d3b725ef78a7b
AngelAbraham <angel-07061@hotmail.com>
2021-05-26 22:55:40 -0500

    Business Layer

---
+++ AmigosCode/src/main/java/com/example/demo/Student/StudentServices.java
@@ -0,0 +1,16 @@
+package com.example.demo.Student;
+
+import java.util.List;
+
+public class StudentServices {
+public List<Student> getStudent() {
+		
+		return List.of(new Student(
+				21L, 
+				"adasd",
+				"asdad", 
+				null, 
+				23));
+	}
+
+}
