b092bd4376050a73d828151ceb9065860061b8b8
AngelAbraham <angel-07061@hotmail.com>
2021-05-26 22:19:28 -0500

    Students constructor

--- AmigosCode/src/main/java/com/example/demo/AmigosCodeApplication.java
+++ AmigosCode/src/main/java/com/example/demo/AmigosCodeApplication.java
@@ -1,11 +1,14 @@
 package com.example.demo;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Student.Student;
 
 @SpringBootApplication
 @RestController
@@ -16,7 +19,13 @@ public class AmigosCodeApplication {
 	}
 
 	@GetMapping
-	public List<String> hello() {
-		return List.of("Hello","World");
+	public List<Student> hello() {
+		
+		return List.of(new Student(
+				21L, 
+				"adasd",
+				"asdad", 
+				null, 
+				23));
 	}
 }
